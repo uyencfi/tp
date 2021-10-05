782b73c53f5557ddd5d8824c286099669d67e2b4
Fergus Mok <fergusmok1@gmail.com>
2021-10-06 05:26:03 +0800

    Update AddressBookParser

--- src/test/java/seedu/address/logic/parser/EdRecordParserTest.java
+++ src/test/java/seedu/address/logic/parser/EdRecordParserTest.java
@@ -29,9 +29,9 @@ import seedu.address.testutil.EditPersonDescriptorBuilder;
 import seedu.address.testutil.PersonBuilder;
 import seedu.address.testutil.PersonUtil;
 
-public class AddressBookParserTest {
+public class EdRecordParserTest {
 
-    private final AddressBookParser parser = new AddressBookParser();
+    private final EdRecordParser parser = new EdRecordParser();
 
     @Test
     public void parseCommand_add() throws Exception {
