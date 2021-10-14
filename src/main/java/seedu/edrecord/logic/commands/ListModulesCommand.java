d7b9d49dffd715ef708ee6309605d664bbbd7c42
Cai Kaian <caipngg@gmail.com>
2021-10-15 00:20:10 +0800

    Add ListModulesCommand

---
+++ src/main/java/seedu/edrecord/logic/commands/ListModulesCommand.java
@@ -0,0 +1,36 @@
+package seedu.edrecord.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import seedu.edrecord.model.Model;
+import seedu.edrecord.model.ReadOnlyModuleSystem;
+import seedu.edrecord.model.module.Module;
+
+/**
+ * Lists all modules in the module system to the user.
+ */
+public class ListModulesCommand extends Command {
+
+    public static final String COMMAND_WORD = "lsmods";
+    public static final String MESSAGE_SUCCESS = "Here are your modules: %s.";
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+
+        ReadOnlyModuleSystem moduleSystem = model.getModuleSystem();
+        requireNonNull(moduleSystem);
+
+        List<String> moduleStringList = new ArrayList<>();
+        for (Module m : moduleSystem.getModuleList()) {
+            moduleStringList.add(m.toString());
+        }
+
+        String moduleList = String.join(", ", moduleStringList);
+        return new CommandResult(String.format(MESSAGE_SUCCESS, moduleList));
+    }
+
+}
